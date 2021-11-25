6902c78b1dd63ad93d4d1906d816c056fbc2ea3b
arulabdulaziz <rulz.matrixs@gmail.com>
2021-11-25 18:12:37 +0700

    add transaction to create and update book

--- app/Controllers/BooksController.ts
+++ app/Controllers/BooksController.ts
@@ -2,14 +2,17 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Response from 'App/Helpers/Response'
 import Book from 'App/Models/Book'
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
-// import Database from '@ioc:Adonis/Lucid/Database'
+import Database from '@ioc:Adonis/Lucid/Database'
 import AuthorsController from 'App/Controllers/AuthorsController'
 import Author from 'App/Models/Author'
 import Category from 'App/Models/Category'
 import BookCategory from 'App/Models/BookCategory'
 /**
- * https://docs.adonisjs.com/guides/models/relationships#attach
- * https://docs.adonisjs.com/reference/orm/base-model#static-firstornew
+ * https://stackoverflow.com/questions/55333574/how-to-use-db-transaction-comit-rollback-in-adonis-js/55461986
+ * buat publisher_id di table book
+ * buat table Publisher : name
+ * buat CRUD publisher
+ * relasi dengan book
  */
 export default class BooksController {
   static responseBookNotFound() {
@@ -37,46 +40,56 @@ export default class BooksController {
   public async create({}: HttpContextContract) {}
 
   public async store({ request, response }: HttpContextContract) {
-    const newPostSchema = schema.create({
-      title: schema.string({ trim: true }, [
-        rules.unique({ table: 'books', column: 'title', where: { deleted_at: null } }),
-      ]),
-      title_arr: schema.string.optional(),
-      categories: schema.array().members(schema.string()),
-      author_id: schema.string(),
-    })
-    await request.validate({
-      schema: newPostSchema,
-      messages: {
-        'required': 'The {{ field }} is required',
-        'title.unique': 'Title already exist',
-        'categories.*.string': 'The categories must be an array of string',
-      },
-    })
-    const { title, title_arr, author_id, categories } = request.body()
-    const author = await Author.find(author_id)
-    const categoriesReq = Array()
-    if (!author) return response.status(400).json(AuthorsController.responseAuthorNotFound())
-    const newBook = await Book.create({
-      title,
-      title_arr,
-      author_id,
-    })
-    await newBook.related('categories').detach()
-    for await (const e of categories) {
-      const category = await Category.firstOrNew({ name: e }, { name: e })
-      if (category.$isPersisted) {
-      } else {
-        category.save()
+    await Database.beginGlobalTransaction()
+    try {
+      const newPostSchema = schema.create({
+        title: schema.string({ trim: true }, [
+          rules.unique({ table: 'books', column: 'title', where: { deleted_at: null } }),
+        ]),
+        title_arr: schema.string.optional(),
+        categories: schema.array().members(schema.string()),
+        author_id: schema.string(),
+      })
+      await request.validate({
+        schema: newPostSchema,
+        messages: {
+          'required': 'The {{ field }} is required',
+          'title.unique': 'Title already exist',
+          'categories.*.string': 'The categories must be an array of string',
+        },
+      })
+      const { title, title_arr, author_id, categories } = request.body()
+      const author = await Author.find(author_id)
+      const categoriesReq = Array()
+      if (!author) return response.status(400).json(AuthorsController.responseAuthorNotFound())
+      const newBook = await Book.create({
+        title,
+        title_arr,
+        author_id,
+      })
+      await newBook.related('categories').detach()
+      for await (const e of categories) {
+        const category = await Category.firstOrNew({ name: e }, { name: e })
+        if (category.$isPersisted) {
+        } else {
+          category.save()
+        }
+        // console.log(category, '<<< category')
+        categoriesReq.push(category.id)
       }
-      console.log(category, '<<< category')
-      categoriesReq.push(category.id)
+      await BookCategory.createMany(
+        categoriesReq.map((e) => ({ book_id: newBook.id, category_id: e }))
+      )
+      await newBook.load('categories')
+      await newBook.load('author')
+      await Database.commitGlobalTransaction()
+      return response.status(201).json(Response.successResponseSimple(true, newBook))
+    } catch (error) {
+      await Database.rollbackGlobalTransaction()
+      return response
+        .status(error.status ? error.status : 400)
+        .json(Response.errorResponseSimple(false, [error]))
     }
-    await BookCategory.createMany(
-      categoriesReq.map((e) => ({ book_id: newBook.id, category_id: e }))
-    )
-    await newBook.load('categories')
-    return response.status(201).json(Response.successResponseSimple(true, newBook))
   }
 
   public async show({ request, response }: HttpContextContract) {
@@ -95,53 +108,65 @@ export default class BooksController {
   public async edit({}: HttpContextContract) {}
 
   public async update({ request, response }: HttpContextContract) {
-    const id = request.param('id')
-    const book = await Book.find(id)
-    if (!book) {
-      return response
-        .status(404)
-        .json(Response.errorResponseSimple(false, BooksController.responseBookNotFound()))
-    }
-    const newPostSchema = schema.create({
-      title: schema.string({ trim: true }),
-      title_arr: schema.string.optional(),
-      categories: schema.array().members(schema.string()),
-      author_id: schema.string(),
-    })
-    await request.validate({
-      schema: newPostSchema,
-      messages: {
-        'required': 'The {{ field }} is required',
-        'categories.*.string': 'The categories must be an array of string',
-      },
-    })
-    const { title, title_arr, author_id, categories } = request.body()
-    const isExistBook = await Book.query().where('title', title).first()
-    if (isExistBook && isExistBook.id != id)
-      return response.status(400).json(BooksController.responseAlreadyExistTitle())
-    const author = await Author.find(author_id)
-    if (!author) return response.status(400).json(AuthorsController.responseAuthorNotFound())
-    await book
-      .merge({
-        title,
-        title_arr,
-        author_id,
-      })
-      .save()
-    const categoriesReq = Array()
-    await book.related('categories').detach()
-    for await (const e of categories) {
-      const category = await Category.firstOrNew({ name: e }, { name: e })
-      if (category.$isPersisted) {
-      } else {
-        category.save()
+    await Database.beginGlobalTransaction()
+    try {
+      const id = request.param('id')
+      const book = await Book.find(id)
+      if (!book) {
+        return response
+          .status(404)
+          .json(Response.errorResponseSimple(false, BooksController.responseBookNotFound()))
       }
-      console.log(category, '<<< category')
-      categoriesReq.push(category.id)
+      const newPostSchema = schema.create({
+        title: schema.string({ trim: true }),
+        title_arr: schema.string.optional(),
+        categories: schema.array().members(schema.string()),
+        author_id: schema.string(),
+      })
+      await request.validate({
+        schema: newPostSchema,
+        messages: {
+          'required': 'The {{ field }} is required',
+          'categories.*.string': 'The categories must be an array of string',
+        },
+      })
+      const { title, title_arr, author_id, categories } = request.body()
+      const isExistBook = await Book.query().where('title', title).first()
+      if (isExistBook && isExistBook.id != id)
+        return response.status(400).json(BooksController.responseAlreadyExistTitle())
+      const author = await Author.find(author_id)
+      if (!author) return response.status(400).json(AuthorsController.responseAuthorNotFound())
+      await book
+        .merge({
+          title,
+          title_arr,
+          author_id,
+        })
+        .save()
+      const categoriesReq = Array()
+      await book.related('categories').detach()
+      for await (const e of categories) {
+        const category = await Category.firstOrNew({ name: e }, { name: e })
+        if (category.$isPersisted) {
+        } else {
+          category.save()
+        }
+        // console.log(category, '<<< category')
+        categoriesReq.push(category.id)
+      }
+      await BookCategory.createMany(
+        categoriesReq.map((e) => ({ book_id: book.id, category_id: e }))
+      )
+      await book.load('categories')
+      await book.load("author")
+      await Database.commitGlobalTransaction()
+      return response.status(200).json(Response.successResponseSimple(true, book))
+    } catch (error) {
+      await Database.rollbackGlobalTransaction()
+      return response
+        .status(error.status ? error.status : 400)
+        .json(Response.errorResponseSimple(false, [error]))
     }
-    await BookCategory.createMany(categoriesReq.map((e) => ({ book_id: book.id, category_id: e })))
-    await book.load('categories')
-    return response.status(200).json(Response.successResponseSimple(true, book))
   }
 
   public async destroy({ request, response }: HttpContextContract) {
